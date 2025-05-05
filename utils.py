08cf1f9fab34e4485a284e694653c34bb3174e2c
miriampalmetshofer <miriam.palmetshofer@students.fh-hagenberg.at>
2025-05-05 17:10:01 +0200

    Move functions to utils and enhance pipeline to use video for testing

---
+++ utils.py
@@ -0,0 +1,139 @@
+from keras_facenet import FaceNet
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from mtcnn import MTCNN
+import os
+
+detector = MTCNN()
+embedder = FaceNet()
+
+
+def load_image(image_path: str) -> np.ndarray:
+    image = cv2.imread(image_path)
+    if image is None:
+        raise ValueError(f"Image not found at path: {image_path}")
+    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+
+def detect_face(image: np.ndarray) -> np.ndarray:
+    result = detector.detect_faces(image)[0]
+    x, y, width, height = result['box']
+    x, y = max(0, x), max(0, y)
+    face = image[y:y + height, x:x + width]
+    face = cv2.resize(face, (160, 200))
+    return face
+
+
+def get_face_embedding(face_rgb: np.ndarray):
+    embedding = embedder.embeddings([face_rgb])[0]
+    return embedding
+
+
+def get_enrollment_embeddings(enrollment_folder) -> list:
+    enrollment_files = os.listdir(enrollment_folder)
+    enrollment_embeddings = []
+    for file in enrollment_files:
+        image_path = os.path.join(enrollment_folder, file)
+        image = cv2.imread(image_path)
+        face_embedding = get_face_embedding(image)
+        enrollment_embeddings.append(face_embedding)
+
+    return enrollment_embeddings
+
+
+def test_with_image_folder(folder_path, enrollment_embeddings, enrollment_images, threshold):
+    test_files = os.listdir(folder_path)
+
+    for file in test_files:
+        image_path = os.path.join(folder_path, file)
+        image = load_image(image_path)
+        image = detect_face(image)
+        test_embedding = get_face_embedding(image)
+        plt.imshow(image)
+        plt.show()
+
+        distances = []
+
+        # Compare test embedding with each enrollment embedding
+        for idx, enrollment_embedding in enumerate(enrollment_embeddings):
+            distance = np.linalg.norm(test_embedding - enrollment_embedding)
+            distances.append(distance)
+
+            # Load enrollment image for display
+            enrollment_image = cv2.imread(enrollment_images[idx])
+            enrollment_image_rgb = cv2.cvtColor(enrollment_image, cv2.COLOR_BGR2RGB)
+
+            print(f"Distance with {enrollment_images[idx]}: {distance:.4f}")
+
+        # Find the closest match
+        closest_match_idx = np.argmin(distances)
+        closest_match_distance = distances[closest_match_idx]
+        # display the closest match
+        closest_match_image = cv2.imread(enrollment_images[closest_match_idx])
+        closest_match_image_rgb = cv2.cvtColor(closest_match_image, cv2.COLOR_BGR2RGB)
+        plt.imshow(closest_match_image_rgb)
+
+        if closest_match_distance < threshold:
+            print(f"✅ Match found with {enrollment_images[closest_match_idx]} at distance {closest_match_distance:.4f}")
+        else:
+            print(f"❌ No match with {enrollment_images[closest_match_idx]} at distance {closest_match_distance:.4f}")
+
+        plt.show()
+
+
+def cut_faces_in_enrollment():
+    enrollment_folder = 'data/enrollment_v2/raw/'
+    enrollment_files = os.listdir(enrollment_folder)
+    counter = 0
+
+    for file in enrollment_files:
+        counter += 1
+        image_path = os.path.join(enrollment_folder, file)
+        image = cv2.imread(image_path)
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        try:
+            face = detect_face(image_rgb)
+            cv2.imwrite(f'data/enrollment_v2/face_{counter}.png', cv2.cvtColor(face, cv2.COLOR_RGB2BGR))
+        except Exception as e:
+            print(f"Error embedding face at file {file}: {e}")
+
+
+def process_video_and_get_embedding():
+    video_path = 'data/images/IMG_7109.mp4'
+    cap = cv2.VideoCapture(video_path)
+
+    embeddings = []
+    frame_count = 0
+    skip_frames = 1
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        if frame_count % skip_frames == 0:
+            rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            face = detect_face(rgb_frame)
+
+            # save face image
+            cv2.imwrite(f'data/enrollment/face_{frame_count}.jpg', cv2.cvtColor(face, cv2.COLOR_RGB2BGR))
+
+        frame_count += 1
+
+    cap.release()
+    cv2.destroyAllWindows()
+    return embeddings
+
+
+def get_closest_match_distance(embedding, enrollment_embeddings):
+    distances = []
+
+    for idx, enrollment_embedding in enumerate(enrollment_embeddings):
+        distance = np.linalg.norm(embedding - enrollment_embedding)
+        distances.append(distance)
+
+    closest_match_idx = np.argmin(distances)
+    closest_match_distance = distances[closest_match_idx]
+    return closest_match_distance
