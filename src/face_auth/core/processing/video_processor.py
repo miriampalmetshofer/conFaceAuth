71a959f0f4b8babf5e6af258a369400ac4260410
miriampalmetshofer <miriam.palmetshofer@students.fh-hagenberg.at>
2026-01-08 12:02:13 +0100

    Change log level for less noise

--- src/face_auth/core/processing/video_processor.py
+++ src/face_auth/core/processing/video_processor.py
@@ -56,7 +56,7 @@ class VideoProcessor:
                         logger.warning(f"No face detected at frame {frame_index}")
                         self.debug_saver.save_frame(frame, frame_index, source_name)
 
-                    logger.info(
+                    logger.debug(
                         f"Frame {frame_index}: Predicted State={auth_result.state.value}, "
                         f"Distance={auth_result.distance:.4f}, Risk Score={auth_result.risk_score:.4f}"
                     )
